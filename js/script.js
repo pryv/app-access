f4377b7636ec5ba777f64d95e09daaf0ef532214
Thiebaud <tmodoux@pryv.com>
2017-01-30 14:01:43 +0100

    Reg parsing in url

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ var defaultPermissions = [{
 window.onload = function () {
   var $masterToken = $('#masterToken');
   permissionsAreaState(false);
-  $('#registerUrlText').text(pryv.utility.urls.parseClientURL().hash.replace('#','') ||
+  var url = pryv.utility.urls.parseClientURL().hash;
+  $('#registerUrlText').text(url.substring(url.lastIndexOf('#')+1,url.lastIndexOf('?')) ||
     pryv.Auth.config.registerURL.host);
   $masterToken.prop('checked', false);
   $('#permissionsArea').val(JSON.stringify(defaultPermissions, null, '  '));
